86fcf960089fdff8fa882c3f0eac6d6bdb6a60c4
Algojo <36105680+meorkamil@users.noreply.github.com>
2023-08-20 01:09:26 +0800

    change Upcoming Schedule struct (#2)
    Co-authored-by: Algojo <meorkami97@gmail.com>

--- main.go
+++ main.go
@@ -84,30 +84,56 @@ type CfUpcomingSchedules struct {
 		ID        string    `json:"id"`
 		Name      string    `json:"name"`
 		URL       string    `json:"url"`
+		TimeZone  string    `json:"time_zone"`
 		UpdatedAt time.Time `json:"updated_at"`
 	} `json:"page"`
 	ScheduledMaintenances []struct {
-		CreatedAt       string `json:"created_at"`
-		ID              string `json:"id"`
-		Impact          string `json:"impact"`
+		ID              string    `json:"id"`
+		Name            string    `json:"name"`
+		Status          string    `json:"status"`
+		CreatedAt       time.Time `json:"created_at"`
+		UpdatedAt       time.Time `json:"updated_at"`
+		MonitoringAt    any       `json:"monitoring_at"`
+		ResolvedAt      any       `json:"resolved_at"`
+		Impact          string    `json:"impact"`
+		Shortlink       string    `json:"shortlink"`
+		StartedAt       time.Time `json:"started_at"`
+		PageID          string    `json:"page_id"`
 		IncidentUpdates []struct {
-			Body       string `json:"body"`
-			CreatedAt  string `json:"created_at"`
-			DisplayAt  string `json:"display_at"`
-			ID         string `json:"id"`
-			IncidentID string `json:"incident_id"`
-			Status     string `json:"status"`
-			UpdatedAt  string `json:"updated_at"`
+			ID                 string    `json:"id"`
+			Status             string    `json:"status"`
+			Body               string    `json:"body"`
+			IncidentID         string    `json:"incident_id"`
+			CreatedAt          time.Time `json:"created_at"`
+			UpdatedAt          time.Time `json:"updated_at"`
+			DisplayAt          time.Time `json:"display_at"`
+			AffectedComponents []struct {
+				Code      string `json:"code"`
+				Name      string `json:"name"`
+				OldStatus string `json:"old_status"`
+				NewStatus string `json:"new_status"`
+			} `json:"affected_components"`
+			DeliverNotifications bool `json:"deliver_notifications"`
+			CustomTweet          any  `json:"custom_tweet"`
+			TweetID              any  `json:"tweet_id"`
 		} `json:"incident_updates"`
-		MonitoringAt   any    `json:"monitoring_at"`
-		Name           string `json:"name"`
-		PageID         string `json:"page_id"`
-		ResolvedAt     any    `json:"resolved_at"`
-		ScheduledFor   string `json:"scheduled_for"`
-		ScheduledUntil string `json:"scheduled_until"`
-		Shortlink      string `json:"shortlink"`
-		Status         string `json:"status"`
-		UpdatedAt      string `json:"updated_at"`
+		Components []struct {
+			ID                 string    `json:"id"`
+			Name               string    `json:"name"`
+			Status             string    `json:"status"`
+			CreatedAt          time.Time `json:"created_at"`
+			UpdatedAt          time.Time `json:"updated_at"`
+			Position           int       `json:"position"`
+			Description        any       `json:"description"`
+			Showcase           bool      `json:"showcase"`
+			StartDate          any       `json:"start_date"`
+			GroupID            string    `json:"group_id"`
+			PageID             string    `json:"page_id"`
+			Group              bool      `json:"group"`
+			OnlyShowIfDegraded bool      `json:"only_show_if_degraded"`
+		} `json:"components"`
+		ScheduledFor   time.Time `json:"scheduled_for"`
+		ScheduledUntil time.Time `json:"scheduled_until"`
 	} `json:"scheduled_maintenances"`
 }
 

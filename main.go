119126a6036a6bd68b642b4eb32f7afb70335ff2
Algojo <meorkami97@gmail.com>
2023-08-20 03:07:19 +0800

    remove interval

--- main.go
+++ main.go
@@ -2,46 +2,39 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 
-	fetchMetrics(10)
+	fetchMetrics()
 }
 
-func fetchMetrics(interval int) {
+func fetchMetrics() {
 
-	for {
+	// CF Summary
+	dataComponents := getCfComponents("https://www.cloudflarestatus.com/api/v2/summary.json")
 
-		// CF Summary
-		dataComponents := getCfComponents("https://www.cloudflarestatus.com/api/v2/summary.json")
+	for _, component := range dataComponents.Components {
 
-		for _, component := range dataComponents.Components {
+		fmt.Printf("%s |  %s\n", component.Name, component.Status)
+	}
 
-			fmt.Printf("%s |  %s\n", component.Name, component.Status)
+	// CF Status
+	cfStatus := getCfStatus("https://www.cloudflarestatus.com/api/v2/status.json")
+	fmt.Println(cfStatus.Status.Indicator)
+
+	// CF Unresolve
+	cfUnresolve := getCfUnresolve("https://www.cloudflarestatus.com/api/v2/incidents/unresolved.json")
+	fmt.Println(cfUnresolve)
+
+	// CF Schedules
+	cfSchedules := getCfSchedules("https://www.cloudflarestatus.com/api/v2/scheduled-maintenances/upcoming.json")
+
+	for _, schedules := range cfSchedules.ScheduledMaintenances {
+
+		for _, incidentupdates := range schedules.IncidentUpdates {
+			fmt.Printf("%s | %s \n", incidentupdates.Body, incidentupdates.Status)
 		}
-
-		// CF Status
-		cfStatus := getCfStatus("https://www.cloudflarestatus.com/api/v2/status.json")
-		fmt.Println(cfStatus.Status.Indicator)
-
-		// CF Unresolve
-		cfUnresolve := getCfUnresolve("https://www.cloudflarestatus.com/api/v2/incidents/unresolved.json")
-		fmt.Println(cfUnresolve)
-
-		// CF Schedules
-		cfSchedules := getCfSchedules("https://www.cloudflarestatus.com/api/v2/scheduled-maintenances/upcoming.json")
-
-		for _, schedules := range cfSchedules.ScheduledMaintenances {
-
-			for _, incidentupdates := range schedules.IncidentUpdates {
-				fmt.Printf("%s | %s \n", incidentupdates.Body, incidentupdates.Status)
-			}
-		}
-
-		time.Sleep(time.Millisecond * 1000)
-
 	}
 
 }

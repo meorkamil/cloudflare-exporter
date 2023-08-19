b4af070f1ce8341f11f05db5020fe1cbdb002469
Algojo <meorkami97@gmail.com>
2023-08-20 00:22:32 +0800

    move summary to components

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-type CfSummary struct {
+type CfComponents struct {
 	Page struct {
 		ID        string    `json:"id"`
 		Name      string    `json:"name"`
@@ -33,59 +33,6 @@ type CfSummary struct {
 		OnlyShowIfDegraded bool      `json:"only_show_if_degraded"`
 		Components         []string  `json:"components,omitempty"`
 	} `json:"components"`
-	Incidents             []any `json:"incidents"`
-	ScheduledMaintenances []struct {
-		ID              string    `json:"id"`
-		Name            string    `json:"name"`
-		Status          string    `json:"status"`
-		CreatedAt       time.Time `json:"created_at"`
-		UpdatedAt       time.Time `json:"updated_at"`
-		MonitoringAt    any       `json:"monitoring_at"`
-		ResolvedAt      any       `json:"resolved_at"`
-		Impact          string    `json:"impact"`
-		Shortlink       string    `json:"shortlink"`
-		StartedAt       time.Time `json:"started_at"`
-		PageID          string    `json:"page_id"`
-		IncidentUpdates []struct {
-			ID                 string    `json:"id"`
-			Status             string    `json:"status"`
-			Body               string    `json:"body"`
-			IncidentID         string    `json:"incident_id"`
-			CreatedAt          time.Time `json:"created_at"`
-			UpdatedAt          time.Time `json:"updated_at"`
-			DisplayAt          time.Time `json:"display_at"`
-			AffectedComponents []struct {
-				Code      string `json:"code"`
-				Name      string `json:"name"`
-				OldStatus string `json:"old_status"`
-				NewStatus string `json:"new_status"`
-			} `json:"affected_components"`
-			DeliverNotifications bool `json:"deliver_notifications"`
-			CustomTweet          any  `json:"custom_tweet"`
-			TweetID              any  `json:"tweet_id"`
-		} `json:"incident_updates"`
-		Components []struct {
-			ID                 string    `json:"id"`
-			Name               string    `json:"name"`
-			Status             string    `json:"status"`
-			CreatedAt          time.Time `json:"created_at"`
-			UpdatedAt          time.Time `json:"updated_at"`
-			Position           int       `json:"position"`
-			Description        any       `json:"description"`
-			Showcase           bool      `json:"showcase"`
-			StartDate          any       `json:"start_date"`
-			GroupID            string    `json:"group_id"`
-			PageID             string    `json:"page_id"`
-			Group              bool      `json:"group"`
-			OnlyShowIfDegraded bool      `json:"only_show_if_degraded"`
-		} `json:"components"`
-		ScheduledFor   time.Time `json:"scheduled_for"`
-		ScheduledUntil time.Time `json:"scheduled_until"`
-	} `json:"scheduled_maintenances"`
-	Status struct {
-		Indicator   string `json:"indicator"`
-		Description string `json:"description"`
-	} `json:"status"`
 }
 
 type CfStatus struct {
@@ -107,9 +54,9 @@ func main() {
 	// TODO move to interface
 
 	// CF Summary
-	dataSummary := getCfSummary("https://www.cloudflarestatus.com/api/v2/summary.json")
+	dataComponents := getCfComponents("https://www.cloudflarestatus.com/api/v2/summary.json")
 
-	for _, component := range dataSummary.Components {
+	for _, component := range dataComponents.Components {
 
 		fmt.Printf("%s |  %s\n", component.Name, component.Status)
 	}
@@ -120,7 +67,7 @@ func main() {
 
 }
 
-func getCfSummary(url string) CfSummary {
+func getCfComponents(url string) CfComponents {
 
 	res, err := http.Get(url)
 
@@ -131,15 +78,15 @@ func getCfSummary(url string) CfSummary {
 
 	defer res.Body.Close()
 
-	var summary CfSummary
+	var component CfComponents
 
 	body, err := ioutil.ReadAll(res.Body)
 
-	if err := json.Unmarshal(body, &summary); err != nil {
+	if err := json.Unmarshal(body, &component); err != nil {
 		fmt.Println("Failed to unmarshal JSON")
 	}
 
-	return summary
+	return component
 
 }
 

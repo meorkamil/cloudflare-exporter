ae2830f790a83566374e872aa1c86c80644cd0d3
Algojo <36105680+meorkamil@users.noreply.github.com>
2023-08-20 15:20:38 +0800

    Create dummy prometheus metrics (#6)
    * update something
    
    * update something
    
    * create dummpy metrics
    
    ---------
    
    Co-authored-by: Algojo <meorkami97@gmail.com>

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 
 	fetchMetrics()
+
 }
 
 func fetchMetrics() {
@@ -16,12 +17,25 @@ func fetchMetrics() {
 
 	for _, component := range dataComponents.Components {
 
-		fmt.Printf("%s |  %s\n", component.Name, component.Status)
+		if component.Status != "operational" {
+			fmt.Printf("cf_components_status{componet='%s'}0\n", component.Name)
+		} else {
+			fmt.Printf("cf_components_status{componet='%s'}1\n", component.Name)
+		}
 	}
 
 	// CF Status
 	cfStatus := getCfStatus("https://www.cloudflarestatus.com/api/v2/status.json")
-	fmt.Println(cfStatus.Status.Indicator)
+
+	if cfStatus.Status.Indicator == "minor" {
+		fmt.Printf("cf_status{='https://www.cloudflarestatus.com/api/v2/status.json'}2\n")
+	} else if cfStatus.Status.Indicator == "major" {
+		fmt.Printf("cf_status{='https://www.cloudflarestatus.com/api/v2/status.json'}1\n")
+	} else if cfStatus.Status.Indicator == "crtical" {
+		fmt.Printf("cf_status{='https://www.cloudflarestatus.com/api/v2/status.json'}0\n")
+	} else {
+		fmt.Printf("cf_status{='https://www.cloudflarestatus.com/api/v2/status.json'}3\n")
+	}
 
 	// CF Unresolve
 	cfUnresolve := getCfUnresolve("https://www.cloudflarestatus.com/api/v2/incidents/unresolved.json")
@@ -33,7 +47,7 @@ func fetchMetrics() {
 	for _, schedules := range cfSchedules.ScheduledMaintenances {
 
 		for _, incidentupdates := range schedules.IncidentUpdates {
-			fmt.Printf("%s | %s \n", incidentupdates.Body, incidentupdates.Status)
+			fmt.Printf("cf_schedules{descripton='%s'}1 \n", incidentupdates.Body)
 		}
 	}
 

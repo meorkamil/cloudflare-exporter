cbb11e3a63ffa69cf9a57c24e2219ae33c461c18
Algojo <36105680+meorkamil@users.noreply.github.com>
2023-08-20 16:35:28 +0800

    Prometheus metrics (#7)
    * add prometheus metrics
    
    * set all metrics
    
    ---------
    
    Co-authored-by: Algojo <meorkami97@gmail.com>

--- main.go
+++ main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 
-	fetchMetrics()
+	//fetchMetrics()
+	// Create a non-global registry.
+	reg := prometheus.NewRegistry()
+
+	// Create new metrics and register them using the custom registry.
+	m := NewMetrics(reg)
+	// Set values for the new created metrics.
+	m.cfStatusMetric.Set(65)
+
+	// Expose metrics and custom registry via an HTTP server
+	// using the HandleFor function. "/metrics" is the usual endpoint for that.
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
+	log.Fatal(http.ListenAndServe(":50001", nil))
 
 }
 

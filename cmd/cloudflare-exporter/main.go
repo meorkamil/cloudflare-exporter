a876173085eb628f4a2efb8f7f9a417c57abbef9
Kamil Sulaiman <meorkamil97@gmail.com>
2024-09-28 11:33:10 +0800

    v:1.2.0 bug fixes for metrics scrape
    bug fixes metrics scrape
    
    Fixing metrics bugs
    
    change build command and port

---
+++ cmd/cloudflare-exporter/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"cloudflare-status/internal/metrics"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func main() {
+
+	metrics.RecordMetrics()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":5001", nil)
+}
--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,36 +1,67 @@
 package metrics
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"cloudflare-status/internal/cloudflare"
+	"log"
+	"time"
 
-type metrics struct {
-	CfSummaryMetric   prometheus.Gauge
-	CfIncidentMetric  *prometheus.GaugeVec
-	CfComponentMetric *prometheus.GaugeVec
-}
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
 
-func NewMetrics(reg prometheus.Registerer) *metrics {
-	m := &metrics{
-		CfSummaryMetric: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "cloudflare_exporter_summary",
-			Help: "Current Cloudflare Summary",
-		}),
-		CfIncidentMetric: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "cloudflare_exporter_incident",
-				Help: "Current Cloudflare incident",
-			},
-			[]string{"name"},
-		),
-		CfComponentMetric: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "cloudflare_exporter_component",
-				Help: "Current Cloudflare Component",
-			},
-			[]string{"name"},
-		),
-	}
-	reg.MustRegister(m.CfSummaryMetric)
-	reg.MustRegister(m.CfIncidentMetric)
-	reg.MustRegister(m.CfComponentMetric)
-	return m
+var (
+	OpsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+	CfSumMetric = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "cloudflare_exporter_summary",
+		Help: "Current cloudflare summary",
+	})
+	CfIncMetric = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloudflare_exporter_incident",
+		Help: "Current cloudflare incident",
+	},
+		[]string{"name"},
+	)
+	CfComMetric = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloudflare_exporter_component",
+		Help: "Current cloudflare component status",
+	},
+		[]string{"name"},
+	)
+)
+
+func RecordMetrics() {
+	go func() {
+		for {
+			incidents := cloudflare.CfIncidents()
+			components := cloudflare.CfComponents()
+			CfSumMetric.Set(cloudflare.CfSummaries())
+
+			for _, s := range components.Components {
+
+				log.Println("cloudflare component", s.Name)
+
+				if s.Status != "operational" {
+					CfComMetric.With(prometheus.Labels{"name": s.Name}).Set(1)
+				} else {
+					CfComMetric.With(prometheus.Labels{"name": s.Name}).Set(0)
+				}
+			}
+
+			for _, s := range incidents.Incidents {
+
+				log.Println("cloudflare incident:", s.Name)
+
+				if s.Status != "resolved" {
+					CfIncMetric.With(prometheus.Labels{"name": s.Name}).Set(1)
+				} else {
+					CfIncMetric.With(prometheus.Labels{"name": s.Name}).Set(0)
+				}
+			}
+
+			time.Sleep(5 * time.Second)
+		}
+	}()
 }
